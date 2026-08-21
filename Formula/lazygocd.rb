1b7afd05461057c6efdbf52c97c56bf2f4d0b487
Sahil Chalke <chalke.sahil1015@gmail.com>
2026-08-21 16:40:07 +0530

    lazygocd: bottle for arm64_tahoe

--- Formula/lazygocd.rb
+++ Formula/lazygocd.rb
@@ -5,6 +5,12 @@ class Lazygocd < Formula
   sha256 "0901cf6724abf08cc004331924e6e3213e2e81f99425f3ddbbf6610d40b56b23"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/Sahilll15/lazygocd/releases/download/v0.3.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "49d42926ae2bf38231194ad16b2484f4786ef67e7a667a6bd98c3531fdaf07d9"
+  end
+
   depends_on "rust" => :build
 
   def install
