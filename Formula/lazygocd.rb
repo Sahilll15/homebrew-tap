cbc8554b82a493b23ca773ef8b1a0f5ec139e794
Sahil Chalke <chalke.sahil1015@gmail.com>
2026-08-25 12:40:27 +0530

    lazygocd 0.10.1

--- Formula/lazygocd.rb
+++ Formula/lazygocd.rb
@@ -1,15 +1,10 @@
 class Lazygocd < Formula
   desc "Fast, keyboard-driven terminal UI for GoCD"
   homepage "https://github.com/Sahilll15/lazygocd"
-  url "https://github.com/Sahilll15/lazygocd/archive/refs/tags/v0.10.0.tar.gz"
-  sha256 "8285a7abccc0e44a6d2d68702acf1926a30a70297664a1fa491dab3cecbbb4ef"
+  url "https://github.com/Sahilll15/lazygocd/archive/refs/tags/v0.10.1.tar.gz"
+  sha256 "eaa7b0c801310a2c4b25a5ebdba279a50c8f4fb7e6d71727324ac836b680782a"
   license "MIT"
 
-  bottle do
-    root_url "https://github.com/Sahilll15/lazygocd/releases/download/v0.10.0"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "72b9078be96e8ab4d755dbb2e193c9017c8242eae1d62cd2ea0b6d3675e2f186"
-  end
 
 
 
