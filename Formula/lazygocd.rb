674fdb1e81cceb91bc1362461c301f504147f1ac
Sahil Chalke <chalke.sahil1015@gmail.com>
2026-08-26 16:20:27 +0530

    lazygocd: refresh source sha256 after the v0.10.7 tag moved

--- Formula/lazygocd.rb
+++ Formula/lazygocd.rb
@@ -2,7 +2,7 @@ class Lazygocd < Formula
   desc "Fast, keyboard-driven terminal UI for GoCD"
   homepage "https://github.com/Sahilll15/lazygocd"
   url "https://github.com/Sahilll15/lazygocd/archive/refs/tags/v0.10.7.tar.gz"
-  sha256 "9a3f0dab521f6d261afcb4cd11890fb0d63cc78ec7325005a74c1b84ed068335"
+  sha256 "7a16d00405fe5184c36ab04620c55f9d6cdacc40c28cc91a0be83b485580d947"
   license "MIT"
 
   bottle do
