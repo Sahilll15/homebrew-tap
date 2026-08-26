b6787fd3ead7eb7334b8d037aaf8b6ca92b72270
Sahil Chalke <chalke.sahil1015@gmail.com>
2026-08-26 16:06:04 +0530

    lazygocd: bottle 0.10.7

--- Formula/lazygocd.rb
+++ Formula/lazygocd.rb
@@ -5,6 +5,11 @@ class Lazygocd < Formula
   sha256 "9a3f0dab521f6d261afcb4cd11890fb0d63cc78ec7325005a74c1b84ed068335"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/Sahilll15/lazygocd/releases/download/v0.10.7"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "d87ae9edc31d97e13486a00b9bdf2c4715879efb53d0299c7dbcbd0a8b06cb93"
+  end
+
 
 
 
