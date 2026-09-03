dcaea12e7d82a1befbd447a6d0b0ac68c33a8b60
Sahil Chalke <chalke.sahil1015@gmail.com>
2026-09-03 18:33:36 +0530

    lazygocd 0.10.8

--- Formula/lazygocd.rb
+++ Formula/lazygocd.rb
@@ -1,17 +1,14 @@
 class Lazygocd < Formula
   desc "Fast, keyboard-driven terminal UI for GoCD"
   homepage "https://github.com/Sahilll15/lazygocd"
-  url "https://github.com/Sahilll15/lazygocd/archive/refs/tags/v0.10.7.tar.gz"
-  sha256 "7a16d00405fe5184c36ab04620c55f9d6cdacc40c28cc91a0be83b485580d947"
+  url "https://github.com/Sahilll15/lazygocd/archive/refs/tags/v0.10.8.tar.gz"
+  sha256 "b6b956283b723f253dbe5045c9bcb2a5e44fff5485dd732677e1b9292dbc1b07"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/Sahilll15/lazygocd/releases/download/v0.10.7"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "d87ae9edc31d97e13486a00b9bdf2c4715879efb53d0299c7dbcbd0a8b06cb93"
+    root_url "https://github.com/Sahilll15/lazygocd/releases/download/v0.10.8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "aa33f8b1f9bc15a07448be6235df71713d8f515e567914b22be8bd9f3435c7f4"
   end
-
-
-
 
   depends_on "rust" => :build
 
